266771431f6f63c82c6df7feb1a9615e966e34df
Deidre <deidre.balard@outlook.com>
2020-02-19 19:05:57 -0500

    Commit

---
+++ Exercice Chapter 3 pag 39/Exercice Chapter 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Chapter_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myString = "\"myInteger\"is";
+            myInteger = 17;
+            Console.WriteLine("{0} {1}.", myString, myInteger);
+            Console.ReadKey();
+        }
+    }
+}
